940b9ce29966521e7c7cb8914159fb3dc963da23
Arifalam786 <arifalam3101@gmail.com>
2023-01-29 20:27:36 +0530

    change 2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import numpy as np
-from sklearn.ensemble import RandomForestRegressor,GradientBoostingRegressor,AdaBoostRegressor,ExtraTreesRegressor
+from sklearn.ensemble import ExtraTreesRegressor
 
 
 
